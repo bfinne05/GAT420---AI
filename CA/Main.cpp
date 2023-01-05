3ebf836c2b4aa41422855f921a4aa232cb2886a3
bfinne05 <100608199+bfinne05@users.noreply.github.com>
2023-01-05 14:12:39 -0700

    made code look neater

--- CA/Main.cpp
+++ CA/Main.cpp
@@ -6,9 +6,6 @@
 void DisplayCellsRow(const std::vector<bool>& cells)
 {
 	// display a row of cells to the console 
-	// display a 'X' if the cell is alive (true) or a '-' if dead (false) 
-	// place a space between characters 
-	// example: - - X X - - X - - - X - - 
 	for (auto cell : cells)
 	{
 		if (cell == true)
@@ -27,24 +24,16 @@ void DisplayCellsRow(const std::vector<bool>& cells)
 std::vector<bool> ProcessCells(const std::vector<bool>& cells, const std::array<bool, 8>& rule)
 {
 	// create cells that will store the cell states after being processed 
-	// use constructor that takes the number of elements to create 
 	std::vector<bool> processed(cells.size());
 
 	for (int i = 0; i < processed.size(); i++)
 	{
 		// get the current and neighboring cell states using (i)ndex 
-		// left = i - 1 
-		// center = i 
-		// right = i + 1 
 		int left = i - 1;
 		if (left < 0) left = cells.size() - 1;
 		int center = i;
 		int right = (i + 1) % cells.size();
 
-
-	   // wrap left and right index if past the edges (left < 0 or right >= size) 
-		// left = (i - 1) < 0 -> size - 1  
-		// right = (i + 1) == size -> 0 
 		bool states[3];
 		states[0] = cells[left];
 		states[1] = cells[center];
@@ -70,7 +59,7 @@ std::vector<bool> ProcessCells(const std::vector<bool>& cells, const std::array<
 		// Rule 7 = 000
 		else if (!states[0] && !states[1] && !states[2]) state = rule[7];
 
-			//<set processed at(i)ndex to state>
+			
 		processed[i] = state;
 	}
 
@@ -80,21 +69,14 @@ std::vector<bool> ProcessCells(const std::vector<bool>& cells, const std::array<
 int main() 
 {
 	srand((unsigned int)time(nullptr));
-	// declare a variable for the number of cells in a row (50) 
+	
 	int numCells = 50;
-		// declare a variable for the number of iterations (40) 
+		
 	int iterations = 40;
 
-		// create vector of cell states boolean (true = alive/false = dead) 
-		// there is a constructor that allows you to provided the number of el
-		//ementsand the default value(int, bool)
-		std::vector<bool> cells(numCells, false);
-	    cells[numCells / 2] = true;
+	std::vector<bool> cells(numCells, false);
+	cells[numCells / 2] = true;
 
-	// set the text to a different color (experiment with different colors 
-	// the values are in hex (0-A) 
-	// the first value is the background and the second the foreground 
-	// https://www.geeksforgeeks.org/how-to-print-colored-text-in-c/ 
 	system("Color 0A");
 
 	// display each iteration 
@@ -117,11 +99,4 @@ int main()
 		cells = ProcessCells(cells, rule188);
 	}
 }
-
-//cells[numCells / 2] = true; 
-//for (int i = 0; i < 5; i++)
-//{
-//	cells[<get random number between 0 and numbers of cells – 1>] = true;
-//}
-
 
